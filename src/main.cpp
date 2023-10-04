2060ea8116a96096fd73588a488f6fe905d08c28
Yuri Istomin <nekocoder.pw@gmail.com>
2023-10-04 14:38:29 +0300

    Add copyrights to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,13 @@
+//===--- main.cpp ---------------------------------------------------------===//
+//
+// Copyright (c) 2023 Yuri Istomin
+//
+// Part of the NekoCdr's "tasknow" project
+// Licensed under the Apache License, Version 2.0 (the "License").
+// See http://www.apache.org/licenses/LICENSE-2.0 for license information.
+//
+//===----------------------------------------------------------------------===//
+
 // NOLINTNEXTLINE
 int main()
 {
