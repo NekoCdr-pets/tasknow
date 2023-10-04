7ee091944bf47d1e65d9dc5b7edc507f2d050182
Yuri Istomin <nekocoder.pw@gmail.com>
2023-10-04 15:10:26 +0300

    Add boost::ut framework (#2)

---
+++ tests/test.cpp
@@ -0,0 +1,19 @@
+//===--- test.cpp - tests entry point  ------------------------------------===//
+//
+// Copyright (c) 2023 Yuri Istomin
+//
+// Part of the NekoCdr's "tasknow" project
+// Licensed under the Apache License, Version 2.0 (the "License").
+// See http://www.apache.org/licenses/LICENSE-2.0 for license information.
+//
+//===----------------------------------------------------------------------===//
+
+#include <boost/ut.hpp>
+
+using namespace boost::ut;
+
+// NOLINTNEXTLINE
+int main()
+{
+    return 0;
+}
