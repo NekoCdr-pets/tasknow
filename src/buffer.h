31d68a7d6377e1d14767813c9ce4aa7c7faefdce
Yuri Istomin <nekocoder.pw@gmail.com>
2023-10-04 17:24:15 +0300

    Add buffer type and project defines

---
+++ src/buffer.h
@@ -0,0 +1,27 @@
+//===--- buffer.h - Serialized data buffer --------------------------------===//
+//
+// Copyright (c) 2023 Yuri Istomin
+//
+// Part of the NekoCdr's "tasknow" project
+// Licensed under the Apache License, Version 2.0 (the "License").
+// See http://www.apache.org/licenses/LICENSE-2.0 for license information.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef TASKNOW_BUFFER_H
+#define TASKNOW_BUFFER_H
+
+#include "defines.h"
+
+#include <memory>
+
+namespace tasknow {
+
+struct Buffer {
+    Size_t size;
+    std::unique_ptr<unsigned char[]> data;
+};
+
+} // end namespace tasknow
+
+#endif // TASKNOW_BUFFER_H
---
+++ src/defines.h
@@ -0,0 +1,22 @@
+//===--- defines.h - Tasknow global defines & constants -------------------===//
+//
+// Copyright (c) 2023 Yuri Istomin
+//
+// Part of the NekoCdr's "tasknow" project
+// Licensed under the Apache License, Version 2.0 (the "License").
+// See http://www.apache.org/licenses/LICENSE-2.0 for license information.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef TASKNOW_DEFINES_H
+#define TASKNOW_DEFINES_H
+
+#include <cstdint>
+
+namespace tasknow {
+
+using Size_t = int16_t;
+
+} // end namespace tasknow
+
+#endif // TASKNOW_DEFINES_H
