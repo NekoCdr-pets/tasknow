0f87851bcfb152fe7c5939af35beabb89512d68f
Yuri Istomin <nekocoder.pw@gmail.com>
2023-10-05 22:34:17 +0300

    Add cli executable

---
+++ src/tasknow-cli/main.cpp
@@ -0,0 +1,15 @@
+//===--- main.cpp - CLI entrypoint ----------------------------------------===//
+//
+// Copyright (c) 2023 Yuri Istomin
+//
+// Part of the NekoCdr's "tasknow" project
+// Licensed under the Apache License, Version 2.0 (the "License").
+// See http://www.apache.org/licenses/LICENSE-2.0 for license information.
+//
+//===----------------------------------------------------------------------===//
+
+// NOLINTNEXTLINE
+int main(int argc, char** argv)
+{
+    return 0;
+}
