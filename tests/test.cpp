fb8255468b00ad274fb61a55bee291c4f1f396bb
Yuri Istomin <nekocoder.pw@gmail.com>
2023-10-05 13:33:59 +0300

    Improve test comparison of serialized buffer size with expected size

--- tests/test.cpp
+++ tests/test.cpp
@@ -9,11 +9,12 @@
 //===----------------------------------------------------------------------===//
 
 #include "buffer.h"
+#include "defines.h"
 #include "task.h"
 
 #include <boost/ut.hpp>
 
-#include <cstring>
+#include <string>
 
 using namespace boost::ut;
 
@@ -23,13 +24,15 @@ namespace tn = tasknow;
 int main()
 {
     "task_[un]serializer"_test = [] {
-        const char* str{"abc"};
+        std::string str{"abc"};
         tn::Task task{str};
+
+        size_t buff_size{tn::SizeofST + str.size()};
 
         tn::Buffer serialized_task = tn::serialize(&task);
         tn::Task unserialized_task = tn::unserialize(&serialized_task);
 
-        expect(serialized_task.size == 5_i16);
+        expect(serialized_task.size == buff_size);
         expect(task == unserialized_task);
     };
     
