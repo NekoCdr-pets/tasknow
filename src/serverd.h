4d3892ce0e9b9a102f019464edbfe5b621ac3c75
Yuri Istomin <nekocoder.pw@gmail.com>
2023-10-23 21:35:52 +0300

    Add empty lines to increase readability

--- src/serverd.h
+++ src/serverd.h
@@ -25,25 +25,32 @@ auto init(
     socklen_t addr_len,
     std::string_view sock_path
 ) -> void;
+
 auto create_socket(int* server_sock) -> void;
+
 auto bind_socket(
     int* server_sock,
     struct sockaddr_un* server_addr,
     socklen_t addr_len
 ) -> void;
+
 auto listen_socket(int* server_sock, const int backlog_size) -> void;
+
 auto accept_client(
     int* server_sock,
     int* client_sock,
     struct sockaddr_un* client_addr,
     socklen_t* addr_len
 ) -> void;
+
 auto log_peer_name(
     int* client_sock,
     struct sockaddr_un* client_addr,
     socklen_t* addr_len
 ) -> void;
+
 auto recieve_method(int* client_sock) -> Query_method;
+
 auto handle_request(int* client_sock, Query_method query_method) -> void;
 
 [[noreturn]]
