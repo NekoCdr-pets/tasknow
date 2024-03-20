eed9089b8c7f98c2093c02a4ef0f4275487972a8
Yuri Istomin <nekocoder.pw@gmail.com>
2024-03-20 17:01:06 +0200

    Add test for task list [de]serializer

--- tests/test.cpp
+++ tests/test.cpp
@@ -11,6 +11,7 @@
 #include "buffer.h"
 #include "defines.h"
 #include "task.h"
+#include "task_list.h"
 
 #include <boost/ut.hpp>
 
@@ -35,6 +36,27 @@ int main()
 
         expect(serialized_task.size == buff_size);
         expect(task == unserialized_task);
+    };
+
+    "task_list_(de)serializer"_test = [] {
+        tn::Task_list task_list{};
+        task_list.container.push_back({"abc"});
+        task_list.container.push_back({"xyz"});
+
+        std::ptrdiff_t buff_size{
+            tn::BytesForBufferSize * 2
+            + tn::BytesForSize * 2
+            + std::ssize(task_list.container[0].title)
+            + std::ssize(task_list.container[1].title)
+        };
+
+        tn::Buffer<tn::Task_list> serialized_list{tn::serialize(&task_list)};
+        tn::Task_list unserialized_list{tn::deserialize(&serialized_list)};
+
+        expect(serialized_list.size == buff_size);
+        expect(task_list.container.size() == unserialized_list.container.size());
+        expect(task_list.container[0] == unserialized_list.container[0]);
+        expect(task_list.container[1] == unserialized_list.container[1]);
     };
     
     return 0;
