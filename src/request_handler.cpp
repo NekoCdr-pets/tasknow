6b7a2e3481b7bb104b64d6c7f51a0ea2ac8d70bf
Yuri Istomin <nekocoder.pw@gmail.com>
2024-03-23 22:23:33 +0200

    [serverd] extract receiving of the data into a separate function (#27)
    - Extract receiving of the data from client into a separate serverd function
    - Rename the recieve_method() function to the get_request_method()

--- src/request_handler.cpp
+++ src/request_handler.cpp
@@ -22,6 +22,44 @@
 
 namespace tasknow::request_handler {
 
+auto receive_data(
+    int* client_sock,
+    void* buff,
+    const std::size_t size
+) -> ssize_t {
+    for (int i=1; i<=3; i++) {
+        ssize_t message_length{recv(*client_sock, buff, size, 0)};
+        if (message_length == ErrorCode) {
+            switch (errno) {
+                case EINTR:
+                    if (i==3) {
+                        throw errors::WarningLinuxError{
+                            errno,
+                            "recv(2)",
+                            "The receive was interrupted by delivery of a signal before any data was available."
+                        };
+                    }
+                    continue;
+
+                case ECONNREFUSED:
+                    throw errors::WarningLinuxError{
+                        errno,
+                        "recv(2)",
+                        "A remote host refused to allow the network connection (typically because it is not running the requested service)."
+                    };
+
+                default:
+                    throw errors::UnrecoverableLinuxError{
+                        errno,
+                        "recv(2)",
+                        std::strerror(errno) // NOLINT(concurrency-mt-unsafe)
+                    };
+            }
+        }
+        return message_length;
+    }
+}
+
 auto send_response(
     int* client_sock,
     unsigned char* buff,
